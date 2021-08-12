803f20151855481873bbeb6caf711704a84d08a2
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-12 15:11:32 +0530

    [ruchi]: Added Win or Lose functionality

--- src/com/bridgelabz/gamblerproblem/GamblerProblem.java
+++ src/com/bridgelabz/gamblerproblem/GamblerProblem.java
@@ -1,9 +1,29 @@
 package com.bridgelabz.gamblerproblem;
 
 public class GamblerProblem {
-    public static final int $DAILY = 100;
+    public static int $DAILY = 100;
     public static final int $PERGAME = 1;
+    public static final int WIN = 1;
+    public static final int LOSE = 0;
+
+    public static void WinOrLose()
+    {
+        double Action = Math.floor(Math.random() * 10) % 2;
+        if ((int) Action == WIN) {
+            $DAILY = $DAILY + 1;
+            System.out.println("You won $1 your current stake value is : $" + $DAILY);
+
+        } else if ((int) Action == LOSE) {
+            $DAILY = $DAILY - 1;
+            System.out.println("You lose $1 your current stake value is : $" + $DAILY);
+        }
+    }
+
     public static void main(String[] args) {
 
+        WinOrLose();
+
     }
+
+
 }
